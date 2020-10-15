d692345f5def846825596054d10d305f6247a695
ferrisbueller66 <tmtarpinian@gmail.com>
2020-10-14 23:01:37 -0400

    write Ruby answer code for chapters 25, 26, 27

---
+++ Section-3-Big-O/25 - What_is_good_code.rb
@@ -0,0 +1,11 @@
+nemo = ['nemo']
+
+def findNemo(array)
+    array.each do |i|
+        if i === "nemo"
+            puts "Found NEMO!"
+        end
+    end
+end
+
+findNemo(nemo)
---
+++ Section-3-Big-O/26 - Big_O_and_scalability.rb
@@ -0,0 +1,18 @@
+nemo = ['nemo']
+everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
+
+large = Array.new(100000, 'nemo')
+
+def findNemo(array)
+    t0 = (Time.now.to_f * 1000)
+    array.each do |i|
+        if i === "nemo"
+            puts "Found NEMO!"
+        end
+    end
+
+    t1 = (Time.now.to_f * 1000)
+    puts "Call to find Nemo took #{(t1-t0)*1000.0} milliseconds"
+end
+
+findNemo(large)
