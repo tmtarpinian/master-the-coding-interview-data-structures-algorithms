d84bcac2decdf11b99849e7e00f288bddd7983f8
ferrisbueller66 <tmtarpinian@gmail.com>
2020-10-15 00:47:31 -0400

    write Ruby answer for chapts 29, 31, 33

---
+++ Section-3-Big-O/29 - Exercise_Big_O_calculation.rb
@@ -0,0 +1,13 @@
+#What is the Big O of the below function? (Hint, you may want to go line by line)
+def funChallenge(input)
+    a = 10
+    a = 50 + 3
+
+    input.each do |i|
+        anotherFunction()
+        stranger = true
+        a++
+    end
+    
+    a
+end
