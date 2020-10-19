c759993c5ecfc4131d374c07f86da3b28fab47d0
ferrisbueller66 <tmtarpinian@gmail.com>
2020-10-19 01:20:50 -0400

    write ruby answer code for chapt 69

---
+++ Section-6-Data-Structures-Arrays/69 - Solution_Reverse_a_string.rb
@@ -0,0 +1,24 @@
+str = "Hi My name is Andrei"
+
+def reverse(str)
+    if !str || str.length < 2 || str != str.to_s
+        puts "hmm that is not good"
+    end
+
+    backwards = []
+    total_items = str.length-1
+    while total_items >= 0
+        backwards.push(str[total_items])
+        total_items -= 1
+    end
+    
+    backwards.join('')
+end
+
+def reverse2(str)
+    str.split('').reverse.join('')
+end
+
+
+reverse(str)
+#reverse2(str)
